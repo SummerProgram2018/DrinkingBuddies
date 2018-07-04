2dfc6217101c392b149f98266114690c3a2049b9
Jonah Luton <jluton@iinet.net.au>
2018-07-04 11:22:44 +0800

    -initial

---
+++ app/src/main/java/com/example/jonahluton/drinkingbuddiesjonah/BottomTabs.java
@@ -0,0 +1,50 @@
+package com.example.jonahluton.drinkingbuddiesjonah;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class BottomTabs extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_matching:
+                    mTextMessage.setText(R.string.title_matching);
+                    return true;
+                case R.id.navigation_chat:
+                    mTextMessage.setText(R.string.title_chat);
+                    return true;
+                case R.id.navigation_destination:
+                    mTextMessage.setText(R.string.title_destination);
+                    return true;
+                case R.id.navigation_reviews:
+                    mTextMessage.setText(R.string.title_reviews);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_tabs);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
