24c3d279cc5f09089ebccd04df7b599e2fff071a
Louloutahooloo <l.woodrow@uqconnect.edu.au>
2018-07-08 17:36:32 +1000

    it now has fragments, and you can work on individual tabs without breaking stuff

--- app/src/main/java/com/example/jonahluton/drinkingbuddiesjonah/BottomTabs.java
+++ app/src/main/java/com/example/jonahluton/drinkingbuddiesjonah/BottomTabs.java
@@ -1,36 +1,67 @@
 package com.example.jonahluton.drinkingbuddiesjonah;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-public class BottomTabs extends AppCompatActivity {
+import com.example.jonahluton.drinkingbuddiesjonah.dummy.DummyContent;
 
-    private TextView mTextMessage;
+public class BottomTabs extends AppCompatActivity implements
+        Tab0Matching.OnListFragmentInteractionListener,
+        Tab1Chat.OnListFragmentInteractionListener,
+        Tab2Destination.OnFragmentInteractionListener,
+        Tab3Reviews.OnListFragmentInteractionListener, Tab4Profile.OnFragmentInteractionListener{
+
+    private BottomNavigationView mBottomNavigationView;
+    private FrameLayout mainFrame;
+
+    /*
+    private Tab0Matching matching;
+    private Tab1Chat chat;
+    private Tab2Destination destination;
+    private Tab3Reviews reviews;
+    private Tab4Profile profile;
+    private Fragment focussed;
+    */
+
+
+
+
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            setFragment(new Tab0Matching());
             switch (item.getItemId()) {
                 case R.id.navigation_matching:
-                    mTextMessage.setText(R.string.title_matching);
+                    setFragment(new Tab0Matching());
                     return true;
+
                 case R.id.navigation_chat:
-                    mTextMessage.setText(R.string.title_chat);
+                    setFragment(new Tab1Chat());
                     return true;
+
                 case R.id.navigation_destination:
-                    mTextMessage.setText(R.string.title_destination);
+                    setFragment(new Tab2Destination());
                     return true;
+
                 case R.id.navigation_reviews:
-                    mTextMessage.setText(R.string.title_reviews);
+                    setFragment(new Tab3Reviews());
                     return true;
+
                 case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
+                    setFragment(new Tab4Profile());
                     return true;
             }
             return false;
@@ -42,9 +73,25 @@ public class BottomTabs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_tabs);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mBottomNavigationView= (BottomNavigationView) findViewById(R.id.navigation);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mainFrame = (FrameLayout) findViewById(R.id.content);
     }
 
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    private void setFragment (Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment);
+        fragmentTransaction.commit();
+    }
 }
